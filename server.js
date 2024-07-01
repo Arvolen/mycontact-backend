8cdc30e740cb86634d475c8207eae153c2174727
Arvolen <arlenlie123@gmail.com>
2024-07-01 11:25:26 +0800

    1st

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -7,8 +8,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/contacts', require("./routes/contactRoutes")
-)
-
+);
+app.use(errorHandler);
 
 
 app.listen(port, () =>  {
