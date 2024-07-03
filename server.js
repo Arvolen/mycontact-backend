d2de5c90fbf94978455368a0b7ed673efc85f98d
Arvolen <arlenlie123@gmail.com>
2024-07-03 10:04:17 +0800

    finak
    dawd

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors")
 
 connectDb();
 
 const app = express();
+
 
 const port = process.env.PORT || 5000;
 
@@ -13,7 +15,7 @@ app.use(express.json());
 app.use('/api/contacts', require("./routes/contactRoutes"));
 app.use('/api/users', require("./routes/userRoutes"));
 app.use(errorHandler);
-
+app.use(cors())
 
 app.listen(port, () =>  {
 
