0e3b76931656afe755d400cebed2fbbb58c5e856
Yojimboshi <edan2926@gmail.com>
2024-07-03 10:23:22 +0800

    tested working /api/test

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
-const dotenv = require("dotenv").config();
-const cors = require("cors")
-
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 const allowedOrigins = [
-    'http://localhost:3000', '*'
+    'http://localhost:3000',
+    '*'
 ];
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || process.env.FRONTEND_URL === '*') {
+        if (!origin || allowedOrigins.includes(origin) || process.env.FRONTEND_URL === '*') {
             callback(null, true);
         } else {
-            callback(new CustomError('Not allowed by CORS', 403));
+            callback(new Error('Not allowed by CORS'));
         }
     },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -27,26 +26,24 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
 app.get('/api/test', (req, res) => {
     res.send('Test endpoint is working');
-  });
+});
 
 app.use('/api/contacts', require("./routes/contactRoutes"));
 app.use('/api/users', require("./routes/userRoutes"));
 app.use(errorHandler);
-app.use(cors())
 
 async function startServer() {
     try {
         // Wait for DB connection
-        await connectDb();
+        // await connectDb();
 
         // Start the server only after DB connection is successful
         app.listen(port, () => {
-            console.log(`server running on port ${port}`);
+            console.log(`Server running on port ${port}`);
         });
     } catch (err) {
         console.error('Failed to connect to database:', err);
